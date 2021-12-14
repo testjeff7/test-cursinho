15fb1c0cc607721d6b54a2f670ca11fa67c4fc10
jeffersonb7 <jeffersonbfs7@gmail.com>
2021-12-14 20:47:10 -0300

    ad

--- cursinhoPotengi/settings.py
+++ cursinhoPotengi/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'testemailjeff7@gmail.com'
+EMAIL_HOST_PASSWORD = 'test07jeff_1'
 
-CONTACT_EMAIL = ''
+CONTACT_EMAIL = 'testemailjeff7@gmail.com'
